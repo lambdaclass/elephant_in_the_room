e77d550e8d46aae4d2e86cdc5c9cbec9828785d7
juan bono <juanbono94@gmail.com>
2018-03-02 15:11:23 -0300

    add uikit and related dependencies

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -47,6 +47,11 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: ["node_modules/uikit/src/scss"]
+      }
     }
   },
 
@@ -57,6 +62,11 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: {
+      $: "jquery",
+      uikit: "uikit",
+      icons: "uikit/dist/js/uikit-icons"
+    }
   }
 };
--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,9 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import "phoenix_html";
 
+uikit.use(icons);
 // Import local files
 //
 // Local files can be imported directly using relative
