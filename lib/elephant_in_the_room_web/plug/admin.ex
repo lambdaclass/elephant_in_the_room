020ee21e63e5c157a3e082a86201189a9a52c908
juan bono <juanbono94@gmail.com>
2018-03-06 18:39:11 -0300

    add protected routes

--- lib/elephant_in_the_room_web/plug/admin.ex
+++ lib/elephant_in_the_room_web/plug/admin.ex
@@ -1,8 +1,23 @@
 defmodule ElephantInTheRoomWeb.Plugs.Admin do
   alias Plug.Conn
+  alias ElephantInTheRoomWeb.Router.Helpers
+  use Phoenix.Router
 
-  def on_admin_page(%Conn{} = conn, _) do
-    Conn.assign conn, :admin, true
+  def on_admin_page(%Conn{:private => private} = conn, _) do
+    if Map.has_key?(private, :guardian_default_resource) && role_name(private) == "admin" do
+      role_name = role_name(private)
+
+      conn
+      |> Conn.assign(:role, :admin)
+      |> Conn.assign(:admin, true)
+    else
+      conn
+      |> Phoenix.Controller.redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
   end
-  
+
+  defp role_name(private_field) do
+    private_field.guardian_default_resource.role.name
+  end
 end
