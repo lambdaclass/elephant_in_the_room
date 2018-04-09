07799d77bc787b233621b15b6ae4664507df329f
juan bono <juanbono94@gmail.com>
2018-04-09 11:37:23 -0300

    change author's show template

--- lib/elephant_in_the_room_web/views/author_view.ex
+++ lib/elephant_in_the_room_web/views/author_view.ex
@@ -1,3 +1,10 @@
 defmodule ElephantInTheRoomWeb.AuthorView do
   use ElephantInTheRoomWeb, :view
+  alias ElephantInTheRoom.Repo
+
+  def number_of_published_posts(author) do
+    author
+    |> Repo.preload(:posts)
+    |> (fn author -> length(author.posts) end).()
+  end
 end
