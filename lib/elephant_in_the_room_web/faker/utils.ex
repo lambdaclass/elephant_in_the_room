a9f058f23665ccca0ae99418339495c5f22046ae
hernanex3 <hernan.digiorgi@gmail.com>
2018-06-26 12:55:49 -0300

    make many tries while downloading an image

--- lib/elephant_in_the_room_web/faker/utils.ex
+++ lib/elephant_in_the_room_web/faker/utils.ex
@@ -1,9 +1,19 @@
 defmodule ElephantInTheRoomWeb.Faker.Utils do
   # download the image from source
+  defp download_image(source, trials) when trials < 1 do
+    raise "can not download #{source}"
+  end
+  defp download_image(source, trials) do
+    result = HTTPoison.request(:get, source, "", [], [hackney: [{:follow_redirect, true}]])
+    case result do
+      {:ok, %{body: body}} ->
+        body
+      _ ->
+        download_image(source, trials - 1)
+    end
+  end
   def download_image(source) do
-    {:ok, resp} = HTTPoison.request(:get, source, "", [], [hackney: [{:follow_redirect, true}]])
-    %{body: body} = resp
-    body
+    download_image(source, 8)
   end
 
   def fake_image_upload(attrs, format \\ "png") do
