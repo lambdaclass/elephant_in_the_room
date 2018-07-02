e32856744bb5ddef94fde1c2c73c583093833ff3
juan bono <juanbono94@gmail.com>
2018-07-02 18:33:17 -0300

    add ajax callback

--- assets/js/posts_image_upload.js
+++ assets/js/posts_image_upload.js
@@ -1,13 +1,13 @@
 import $ from "jquery";
 
 $(() => {
-  $("post-edit-content").on("dragover", event => {
+  $(".post-edit-content").on("dragover", event => {
     event.preventDefault();
     event.stopPropagation();
     $(this).addClass("dragging");
   });
 
-  $("post-edit-content").on("dragleave", event => {
+  $(".post-edit-content").on("dragleave", event => {
     event.preventDefault();
     event.stopPropagation();
     $(this).removeClass("dragging");
@@ -16,6 +16,25 @@ $(() => {
   $(".post-edit-content").on("drop", event => {
     event.preventDefault();
     event.stopPropagation();
-    alert("Dropped!");
+    readfile(event.data);
   });
 });
+
+readfile = file => {
+  var formData = new FormData();
+
+  formData.append("file", file);
+
+  $.ajax({
+    url: "./path_to_file_handler.php",
+    type: "POST",
+    data: formData,
+    async: true,
+    success: data => {
+      console.log(data);
+    },
+    cache: false,
+    contentType: false,
+    processData: false
+  });
+};
