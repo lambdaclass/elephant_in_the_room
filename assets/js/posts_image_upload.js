645524c88c32688c540b89b436e41fbe7e31d339
juan bono <juanbono94@gmail.com>
2018-07-03 17:08:50 -0300

    extend to multiple file uploads

--- assets/js/posts_image_upload.js
+++ assets/js/posts_image_upload.js
@@ -3,19 +3,27 @@ const handleFileSelect = evt => {
   evt.preventDefault();
 
   var files = evt.dataTransfer.files;
-
+  var type;
   var reader = new FileReader();
 
   reader.onloadend = () => {
-    sendData(reader.result, files[0].type);
+    sendData(reader.result, type);
   };
 
-  reader.readAsArrayBuffer(files[0]);
+  for (var i = 0, file; (file = files[i]); i++) {
+    type = file.type;
+
+    reader.readAsArrayBuffer(file);
+  }
 };
 
 const sendData = (data, mime) => {
   var XHR = new XMLHttpRequest();
   const url = "http://localhost:4000/images/binary";
+
+  XHR.onloadend = () => {
+    console.log(XHR.response);
+  };
 
   XHR.open("POST", url);
 
