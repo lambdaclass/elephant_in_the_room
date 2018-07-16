a0fd1e679253e1785f2e01a8381764cbf545e722
hernanex3 <hernan.digiorgi@gmail.com>
2018-07-16 17:38:14 -0300

    redirect on tag edition

--- lib/elephant_in_the_room_web/controllers/tag_controller.ex
+++ lib/elephant_in_the_room_web/controllers/tag_controller.ex
@@ -81,7 +81,7 @@ defmodule ElephantInTheRoomWeb.TagController do
       {:ok, tag} ->
         conn
         |> put_flash(:info, "Tag updated successfully.")
-        |> redirect(to: site_tag_path(conn, :show, site, tag))
+        |> redirect(to: tag_path(conn, :public_show, tag.id))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", tag: tag, changeset: changeset)
