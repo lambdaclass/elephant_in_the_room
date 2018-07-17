08e280460e3cad684b03763eac199fcca74e3ac7
Juan Bono <juanbono94@gmail.com>
2018-07-17 17:53:23 -0300

    Add and edit post's creation date  (#279)
    * fix show_date bug and show creation date on post card
    
    * add function to parse the date
    
    * add hour form
    
    * add inserted_at field to changeset and modify hour form builder
    
    * auto complete date
    
    * add default hour function
    
    * add clause to show future dates
    
    * use smaller font on landing page
    
    * add position to post-date

--- lib/elephant_in_the_room_web/utils/utils.ex
+++ lib/elephant_in_the_room_web/utils/utils.ex
@@ -1,8 +1,8 @@
 defmodule ElephantInTheRoomWeb.Utils.Utils do
-
   def get_env(where, name) do
     conf = Application.get_env(:elephant_in_the_room, where)
     value = Keyword.get(conf, name)
+
     case value do
       nil -> raise "no variable #{where}.#{name} defined in configuration"
       value -> value
@@ -11,12 +11,38 @@ defmodule ElephantInTheRoomWeb.Utils.Utils do
 
   def generate_absolute_url(relative_path, conn) do
     scheme = to_string(conn.scheme)
-    port = case {scheme, conn.port} do
-      {"http",  80}  -> ""
-      {"https", 443} -> ""
-      {_, port} -> ":#{port}"
-    end
+
+    port =
+      case {scheme, conn.port} do
+        {"http", 80} -> ""
+        {"https", 443} -> ""
+        {_, port} -> ":#{port}"
+      end
+
     "#{scheme}://#{conn.assigns.site.host}#{port}#{relative_path}"
   end
 
+  def complete_zeros(:date, date) do
+    complete = fn n ->
+      if String.length(n) == 1, do: "0" <> n, else: n
+    end
+
+    year = complete.("#{date.year}")
+    month = complete.("#{date.month}")
+    day = complete.("#{date.day}")
+
+    "#{year}-#{month}-#{day}"
+  end
+
+  def complete_zeros(:hour, date) do
+    complete = fn n ->
+      if String.length(n) == 1, do: "0" <> n, else: n
+    end
+
+    hour = complete.("#{date.hour}")
+    minute = complete.("#{date.minute}")
+    second = complete.("#{date.second}")
+
+    %{hour: hour, minute: minute, second: second}
+  end
 end
