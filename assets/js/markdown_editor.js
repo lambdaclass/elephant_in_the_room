0c638669f8891deea5938ddba659b3facde69a64
Amin Arria <arria.amin@gmail.com>
2018-07-19 10:25:31 -0300

    Fixed trying to insert markdown editor everywhere

--- assets/js/markdown_editor.js
+++ assets/js/markdown_editor.js
@@ -1,8 +1,10 @@
 import InscrybMDE from "inscrybmde"
 
 document.addEventListener("DOMContentLoaded", function() {
-  window.editor = new InscrybMDE({
-    element: document.getElementById("post_content"),
-    spellChecker: false
-  });
+  if(document.getElementById("post_content")) {
+    window.editor = new InscrybMDE({
+      element: document.getElementById("post_content"),
+      spellChecker: false
+    });
+  }
 });
