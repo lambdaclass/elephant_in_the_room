e9af588b3a4e1e4bb19edc47bdf9a11ba30ba002
hernanex3 <hernan.digiorgi@gmail.com>
2018-07-24 12:43:34 -0300

    featured location initial commit

---
+++ lib/elephant_in_the_room_web/controllers/featured_controller.ex
@@ -0,0 +1,10 @@
+defmodule ElephantInTheRoomWeb.FeaturedController do
+  use ElephantInTheRoomWeb, :controller
+  # alias ElephantInTheRoom.Sites.Site
+  #alias ElephantInTheRoom.{Sites, Repo}
+
+  def show_featured_levels(_conn, _params) do
+
+  end
+
+end
