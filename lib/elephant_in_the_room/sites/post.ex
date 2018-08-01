94f97182c4ae5788be58c2b740ee8492c9c9f236
Hernan Ezequiel Di Giorgi <hernan.digiorgi@gmail.com>
2018-08-01 13:41:40 -0300

    Audio & Video inside post (#411)
    * allow iframe embedding
    
    * change video style
    
    * add missing new line

--- lib/elephant_in_the_room/sites/post.ex
+++ lib/elephant_in_the_room/sites/post.ex
@@ -151,7 +151,7 @@ defmodule ElephantInTheRoom.Sites.Post do
   end
 
   def generate_markdown(input) do
-    Cmark.to_html(input, [:safe, :hardbreaks])
+    Cmark.to_html(input, [:hardbreaks])
   end
 
   def parse_categories(params) do
