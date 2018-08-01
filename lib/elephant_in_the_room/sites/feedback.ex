92520111a9219357063d9187905c88823199d62e
juan bono <juanbono94@gmail.com>
2018-08-01 17:51:18 -0300

    show flashes correctly

--- lib/elephant_in_the_room/sites/feedback.ex
+++ lib/elephant_in_the_room/sites/feedback.ex
@@ -16,8 +16,8 @@ defmodule ElephantInTheRoom.Sites.Feedback do
   def changeset(%Feedback{} = feedback, attrs) do
     feedback
     |> cast(attrs, [:email, :text, :site_id])
-    |> validate_required([:text])
-    |> validate_length(:text, max: 300)
-    |> validate_format(:email, ~r/.+@.+\..+/i, message: "El e-mail con formato incorrecto.")
+    |> validate_required([:text], message: "Texto requerido.")
+    |> validate_length(:text, max: 300, message: "La longitud máxima del texto es 300 caracteres.")
+    |> validate_format(:email, ~r/.+@.+\..+/i, message: "E-mail con formato incorrecto.")
   end
 end
--- lib/elephant_in_the_room_web/controllers/feedback_controller.ex
+++ lib/elephant_in_the_room_web/controllers/feedback_controller.ex
@@ -1,29 +1,32 @@
 defmodule ElephantInTheRoomWeb.FeedbackController do
   use ElephantInTheRoomWeb, :controller
-  alias ElephantInTheRoom.{Sites, Sites.Feedback}
+  alias ElephantInTheRoom.{Sites}
 
   def index(%{assigns: %{site: site}} = conn, _params) do
     feedbacks = Sites.list_feedbacks(site)
     render(conn, "index.html", feedbacks: feedbacks)
   end
 
-  def new(%{assigns: %{site: site}} = conn, _params) do
-    changeset =
-      %Feedback{site_id: site.id}
-      |> Sites.change_feedback()
-
-    render(conn, "new.html", changeset: changeset)
-  end
-
   def create(%{assigns: %{site: site}} = conn, params) do
     case Sites.create_feedback(site, params) do
       {:ok, _feedback} ->
         conn
-        |> put_flash(:info, "Feedback creado satisfactoriamente.")
+        |> put_flash(:feedback_ok, "Feedback creado satisfactoriamente.")
         |> redirect(to: site_path(conn, :public_show))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset, site: site)
+        conn
+        |> put_errors(changeset)
+        |> redirect(to: site_path(conn, :public_show))
     end
+  end
+
+
+
+
+  defp put_errors(conn, changeset) do
+    changeset.errors
+    |> Enum.map(fn {k, v} -> {k, (elem(v, 0))} end)
+    |> Enum.reduce(conn, fn ({_k, err}, connection) -> put_flash(connection, :feedback_error, "Error: #{err}") end)
   end
 end
